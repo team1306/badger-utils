4892205d9f9fb2176fb6119032c285575060ac88
Connor Mulligan <cgmulligan33@gmail.com>
2025-12-02 16:34:04 -0600

    Finished cleaning up and made docs

--- lib/src/main/java/badgerutils/motor/MotorUtil.java
+++ lib/src/main/java/badgerutils/motor/MotorUtil.java
@@ -1,37 +1,19 @@
 package badgerutils.motor;
 
 import com.ctre.phoenix6.configs.*;
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
 public class MotorUtil {
-    public static TalonFX initTalonFX(int motorId, NeutralModeValue idleMode) {
-        return initTalonFX(motorId, idleMode, InvertedValue.Clockwise_Positive);
-    }
-
-    public static TalonFX initTalonFX(int motorId, NeutralModeValue idleMode, InvertedValue inverted) {
-        final TalonFX motor = new TalonFX(motorId);
-        final TalonFXConfigurator configurator = motor.getConfigurator();
-
-        final MotorOutputConfigs motorOutputConfig = new MotorOutputConfigs();
-
-        motorOutputConfig.Inverted = inverted;
-        motorOutputConfig.NeutralMode = idleMode;
-
-        configurator.apply(motorOutputConfig);
-
-        return motor;
-    }
-
-    public static TalonFX initTalonFX(int motorId, MotorOutputConfigs motorOutputConfigs, Slot0Configs pidConfigBuilder, CurrentLimitsConfigs currentLimitsConfigs) {
-        final TalonFX motor = new TalonFX(motorId);
-        TalonFXConfigurator configurator = motor.getConfigurator();
-
-        return motor;
-    }
-
+    /// Creates a Slot0Configs object with the desired PID and feedforward gains for a motor.
+    /// @param kP the PID's proportional gain
+    /// @param kI the PID's intergral gain
+    /// @param kD the PID's derivative gain
+    /// @param kS the feedforward's static gain
+    /// @param kV the feedforward's velocity gain
+    /// @param kG the feedforward's gravity gain
+    /// @param kA the feedforward's acceleration gain
+    /// @return A Slot0Configs object which can be applied to the motor configurator.
     public static Slot0Configs pidConfigBuilder(double kP, double kI, double kD, double kS, double kV, double kG, double kA) {
         Slot0Configs config = new Slot0Configs();
         config.kP = kP;
@@ -44,6 +26,10 @@ public class MotorUtil {
         return config;
     }
 
+    /// Creates a MotorOutputConfigs object with the desired inverted and idle mode values.
+    /// @param inverted whether the motor is inverted. Clockwise_Positive is normal; Counterclockwise_Positive is reversed.
+    /// @param idleMode how the motor behaves when no voltage is applied. Options are Brake or Coast.
+    /// @return A MotorOutputConfigs object which can be applied to the motor configurator.
     public static MotorOutputConfigs motorOutputConfigBuilder(InvertedValue inverted, NeutralModeValue idleMode) {
         MotorOutputConfigs config = new MotorOutputConfigs();
         config.Inverted = inverted;
@@ -51,6 +37,10 @@ public class MotorUtil {
         return config;
     }
 
+    /// Creates a CurrentLimitsConfigs object with the desired current limits.
+    /// @param statorCurrentLimit the output current (amps) of the motor
+    /// @param supplyCurrentLimit the maximum current (amps) that can be drawn from the battery
+    /// @return A CurrentLimitsConfigs object which can be applied to the motor configurator.
     public static CurrentLimitsConfigs currentLimitsConfigBuilder(double statorCurrentLimit, double supplyCurrentLimit) {
         CurrentLimitsConfigs config = new CurrentLimitsConfigs();
         config.StatorCurrentLimit = statorCurrentLimit;

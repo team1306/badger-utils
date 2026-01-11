a7461ee66fa30036195e7f528c0d70ad0645e2bc
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2026-01-11 11:14:08 -0600

    update to 2026

--- test-project/src/main/java/frc/robot/Robot.java
+++ test-project/src/main/java/frc/robot/Robot.java
@@ -1,6 +1,5 @@
 package frc.robot;
 
-import badgerlog.Dashboard;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -13,7 +12,6 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic()
     {
-        Dashboard.update();
         CommandScheduler.getInstance().run();
     }
     

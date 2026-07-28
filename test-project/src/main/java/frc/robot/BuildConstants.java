a31cee23ef61f14ce06e44817a9c7686c8f85f0d
Connor Mulligan <cgmulligan33@gmail.com>
2026-07-28 09:30:02 -0500

    build

--- test-project/src/main/java/frc/robot/BuildConstants.java
+++ test-project/src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "test-project";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 81;
-  public static final String GIT_SHA = "60d5a38ec1c5effa78511644598b6dfbd462f099";
-  public static final String GIT_DATE = "2026-07-28 10:07:28 EDT";
+  public static final int GIT_REVISION = 83;
+  public static final String GIT_SHA = "eff46ca738ae456b88603d1a148b960f876b7973";
+  public static final String GIT_DATE = "2026-07-28 10:16:42 EDT";
   public static final String GIT_BRANCH = "follower";
-  public static final String BUILD_DATE = "2026-07-28 10:14:56 EDT";
-  public static final long BUILD_UNIX_TIME = 1785248096145L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-07-28 10:19:36 EDT";
+  public static final long BUILD_UNIX_TIME = 1785248376679L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
